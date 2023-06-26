9a184dc6903ba2485ffbd6ac644a8707b39580d7
NFPerera <niko22602@gmail.com>
2023-06-26 16:55:57 -0300

    enemies added

--- IA-TP2/Assets/_Main/_main/Scripts/zzz/flocking/Leader.cs
+++ IA-TP2/Assets/_Main/_main/Scripts/zzz/flocking/Leader.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using _Main._main.Scripts.Managers;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace _Main._main.Scripts.zzz.flocking
@@ -6,7 +9,13 @@ namespace _Main._main.Scripts.zzz.flocking
     public class Leader : MonoBehaviour, IFlocking
     {
         public float multiplier;
-        public Transform target;
+        [HideInInspector] public Transform target;
+
+        private void Start()
+        {
+            target = GameManager.Instance.GetLocalPlayer().transform;
+        }
+
         public Vector3 GetDir(List<IBoid> boids, IBoid self)
         {
             return (target.position - self.Position).normalized * multiplier;
